71343761be9b112214e8b0052ab45297c6cfbc3a
Andrii Tsvietkov <atsvietkov@student.ucode.world>
2020-12-29 10:37:59 +0200

    libmx.h: printchar, printint, printstr, print_strarr

---
+++ inc/libmx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void mx_printchar(char c);
+void mx_printstr(const char *s);
+void mx_print_strarr(char **arr, const char *delim);
+void mx_printint(int n);
