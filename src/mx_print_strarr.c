ace3c067859330a3d0c96e27d7221b2baa5111bb
Andrii Tsvietkov <atsvietkov@student.ucode.world>
2020-12-29 10:39:42 +0200

    Utils basic: mx_print_strarr

---
+++ src/mx_print_strarr.c
@@ -0,0 +1,27 @@
+#include "libmx.h"
+
+// #include "../inc/libmx.h"
+// void mx_printchar(char c) {
+//     write(1, &c, 1);
+// }
+
+void mx_print_strarr(char **arr, const char *delim) {
+    if (arr != NULL && delim != NULL) {
+        for (int i = 0; arr[i] != NULL; i++) {
+            for (int j = 0; arr[i][j] != '\0'; j++) {
+                write(1, &arr[i][j], 1);
+            }
+            if (arr[i+1] != NULL) {
+                for (int j = 0; delim[j] != '\0'; j++) {
+                    write(1, &delim[j], 1);
+                }
+            }
+        }
+        mx_printchar(10);
+    }
+}
+
+// int main() {
+//     char *arr[] = {"Hello", "mr", "Anderson"};
+//     mx_print_strarr(arr, "----");
+// }
