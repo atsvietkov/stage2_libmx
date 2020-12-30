849b5e1146d06e30a885fcd8996a62e624250a01
Andrii Tsvietkov <atsvietkov@student.ucode.world>
2020-12-30 11:56:03 +0200

    Utils basic: bubble_sort

--- inc/libmx.h
+++ inc/libmx.h
@@ -8,3 +8,5 @@ void mx_printchar(char c);
 void mx_printstr(const char *s);
 void mx_print_strarr(char **arr, const char *delim);
 void mx_printint(int n);
+char *mx_nbr_to_hex(unsigned long nbr);
+int mx_bubble_sort(int *arr, int size);
