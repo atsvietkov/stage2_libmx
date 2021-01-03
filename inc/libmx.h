84ef08828c6eea1b3570b9476a383ed211a54b95
Andii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-03 17:50:16 +0200

    Strings basic: strcmp

--- inc/libmx.h
+++ inc/libmx.h
@@ -26,3 +26,4 @@ int mx_binary_search(char **arr, int size, const char *s, int *count);
 int mx_strlen(const char *s);
 void mx_swap_char(char *s1, char *s2);
 char *mx_strcpy(char *dst, const char *src);
+int mx_strcmp(const char *s1, const char *s2);
