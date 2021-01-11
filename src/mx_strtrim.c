b0f0f88611d5a339bd11ee5a8442e70c1e655a8d
Andrii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-11 14:54:31 +0200

    strtrim fix

--- src/mx_strtrim.c
+++ src/mx_strtrim.c
@@ -7,8 +7,8 @@ char *mx_strtrim(const char *str) {
     else {
         int start_trim = 0;
         int end_trim = 0;
-        for( ; mx_isspace(*str + start_trim); start_trim++);
-        for( ; mx_isspace(*str + mx_strlen(str) - 1 - end_trim); end_trim++);
+        for( ; mx_isspace(str[start_trim]); start_trim++);
+        for( ; mx_isspace(str[mx_strlen(str) - 1 - end_trim]); end_trim++);
 
         char *str_new = mx_strndup(str + start_trim, mx_strlen(str) - start_trim - end_trim);
         return str_new;
