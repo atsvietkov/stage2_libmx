ecf5e543c97358ede11b3b08ea9a25bf4369d6f5
Andrii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-14 16:31:17 +0200

    Memory basic: memcpy

--- src/test_memory.c
+++ src/test_memory.c
@@ -2,9 +2,20 @@
 
 int main() {
     // Memory: basic
-    int arr[10] = { 3,3,3,3,3,3,3,3,3,3 };
-    mx_print_intarr(arr, 10);
-    mx_memset(arr, 0, 10*sizeof(int));
-    mx_print_intarr(arr, 10);
+
+    // int arr[10] = { 3,3,3,3,3,3,3,3,3,3 };
+    // mx_print_intarr(arr, 10);
+    // mx_memset(arr, 0, 10*sizeof(int));
+    // mx_print_intarr(arr, 10);
+
+    int arr1[10];
+    int arr2[10];
+    mx_memset(arr1, 0, 10 * sizeof(int));
+    mx_memset(arr2, 1, 10 * sizeof(int));
+    mx_print_intarr(arr1, 10);
+    mx_print_intarr(arr2, 10);
+    mx_memcpy(arr2, arr1, 10 * sizeof(int));
+    mx_print_intarr(arr1, 10);
+    mx_print_intarr(arr2, 10);
 
 }
