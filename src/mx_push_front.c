0dc6192993de2b68842c9cf8050227a1c3260436
Andii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-18 22:09:54 +0200

    List basic: push_front

---
+++ src/mx_push_front.c
@@ -0,0 +1,12 @@
+#include "libmx.h"
+
+void mx_push_front(t_list **list, void *data) {
+    if (list && data) {
+        t_list *new_node = mx_create_node(data);
+        if(!new_node) {
+            return;
+        }
+        new_node->next = *list;
+        *list = new_node;
+    }
+}
