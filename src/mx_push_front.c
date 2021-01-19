3201a44d51c8a6f6b4ebd80a1a8f8f0a38133057
Andii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-19 20:46:43 +0200

    create_node push_front fix

--- src/mx_push_front.c
+++ src/mx_push_front.c
@@ -1,12 +1,14 @@
 #include "libmx.h"
 
 void mx_push_front(t_list **list, void *data) {
-    if (list && data) {
-        t_list *new_node = mx_create_node(data);
-        if(!new_node) {
-            return;
+    t_list *node = mx_create_node(data);
+    if(node) {
+        if(*list == NULL) {
+            *list = node;
         }
-        new_node->next = *list;
-        *list = new_node;
+        else {
+            node->next = *list;
+            *list = node;
+        }
     }
 }
