f972c770454be956954ce23ba07c11fbdd6f4694
Andii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-20 22:41:16 +0200

    Lists advanced: list_size

--- inc/libmx.h
+++ inc/libmx.h
@@ -84,3 +84,4 @@ void mx_push_front(t_list **list, void *data);
 void mx_push_back(t_list **list, void *data);
 void mx_pop_front(t_list **head);
 void mx_pop_back(t_list **head);
+int mx_list_size(t_list *list);
