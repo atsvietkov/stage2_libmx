615c9d45aea7d49009a8498aa8ec5fe6556577ce
Andii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-20 22:58:17 +0200

    Lists advanced: sort_list

--- inc/libmx.h
+++ inc/libmx.h
@@ -85,3 +85,4 @@ void mx_push_back(t_list **list, void *data);
 void mx_pop_front(t_list **head);
 void mx_pop_back(t_list **head);
 int mx_list_size(t_list *list);
+t_list *mx_sort_list(t_list *lst, bool(*cmp)(void *, void *));
