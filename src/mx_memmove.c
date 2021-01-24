9aa8aaf2c18cab25282e768ac3bb4ab8ffff6f30
Andii Tsvietkov <atsvietkov@student.ucode.world>
2021-01-24 19:49:59 +0200

    End

--- src/mx_memmove.c
+++ src/mx_memmove.c
@@ -1,6 +1,6 @@
 #include "libmx.h"
 
-void *mx_memmove(void*dst, const void*src, size_t len) {
+void *mx_memmove(void *dst, const void *src, size_t len) {
     unsigned char *tmp = malloc(len);
     mx_memcpy(tmp, src, len);
     mx_memcpy(dst, tmp, len);
